e4ec91cc3358745d8aa60ca6fd8c4b77d314f86c
Thomas Schreiner <t.schreiner@gmail.com>
2015-04-08 11:28:23 +0200

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace StopWatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BtnStartStop = new System.Windows.Forms.Button();
+            this.BtnReset = new System.Windows.Forms.Button();
+            this.LblTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnStartStop
+            // 
+            this.BtnStartStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnStartStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnStartStop.Location = new System.Drawing.Point(681, 12);
+            this.BtnStartStop.Name = "BtnStartStop";
+            this.BtnStartStop.Size = new System.Drawing.Size(139, 47);
+            this.BtnStartStop.TabIndex = 0;
+            this.BtnStartStop.Text = "Start";
+            this.BtnStartStop.UseVisualStyleBackColor = true;
+            this.BtnStartStop.Click += new System.EventHandler(this.BtnStartStop_Click);
+            // 
+            // BtnReset
+            // 
+            this.BtnReset.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnReset.Location = new System.Drawing.Point(681, 65);
+            this.BtnReset.Name = "BtnReset";
+            this.BtnReset.Size = new System.Drawing.Size(139, 47);
+            this.BtnReset.TabIndex = 1;
+            this.BtnReset.Text = "Reset";
+            this.BtnReset.UseVisualStyleBackColor = true;
+            this.BtnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // LblTime
+            // 
+            this.LblTime.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblTime.Location = new System.Drawing.Point(13, 13);
+            this.LblTime.Name = "LblTime";
+            this.LblTime.Size = new System.Drawing.Size(662, 100);
+            this.LblTime.TabIndex = 2;
+            this.LblTime.Text = "00:00:00:0000";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 125);
+            this.Controls.Add(this.LblTime);
+            this.Controls.Add(this.BtnReset);
+            this.Controls.Add(this.BtnStartStop);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "StopWatch";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnStartStop;
+        private System.Windows.Forms.Button BtnReset;
+        private System.Windows.Forms.Label LblTime;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer;
+
+namespace StopWatch
+{
+    public partial class Form1 : Form
+    {
+        private DateTime _startTime;
+        private Timer _timer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Reset();
+        }
+
+        private void BtnStartStop_Click(object sender, EventArgs e)
+        {
+            if (BtnStartStop.Text == "Start")
+            {
+                Reset();
+                BtnStartStop.Text = "Stop";
+                _startTime = DateTime.Now;
+                _timer = new Timer();
+                _timer.Tick += Timer_Tick;
+                _timer.Interval = 100;
+                _timer.Start();
+            }
+            else
+            {
+                Stop();
+            }
+        }
+
+        private void Stop()
+        {
+            BtnStartStop.Text = "Start";
+            if(_timer != null)
+                _timer.Stop();
+        }
+
+        void Timer_Tick(object sender, EventArgs e)
+        {
+            TimeSpan diff = _startTime - DateTime.Now;
+            SetTime(diff);
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        private void Reset()
+        {
+            Stop();
+            SetTime(TimeSpan.FromMilliseconds(0));
+        }
+
+        private void SetTime(TimeSpan timeSpan)
+        {
+            LblTime.Text = timeSpan.ToString(@"hh\:mm\:ss\.ffff");
+        }
+    }
+}
